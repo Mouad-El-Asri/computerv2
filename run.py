54591a7aa2ded5ec4b366d7a3f2ab0a604d591c8
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-08-15 15:06:06 +0100

    Initial commit: Setup project structure and add starting code

---
+++ run.py
@@ -0,0 +1,16 @@
+import sys
+from computorv2 import *
+
+def main():
+	print('Welcome to Computorv2, the Python Calculator in command line!\n'
+		'To exit, type "exit" or "quit" and press Enter.')
+	while True:
+		try:
+			user_input: str = input("> ")
+			if user_input in ['exit', 'quit']:
+				break
+		except EOFError:
+			sys.exit('')
+
+if __name__ == '__main__':
+	main()
