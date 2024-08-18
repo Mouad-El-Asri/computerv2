c8368d79e06c5fac1f17f547485e10e00c0b408d
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-08-18 19:57:56 +0100

    Add validation and error handling for multiple variable assignments with syntax checks, and add history

--- computorv2.py
+++ computorv2.py
@@ -4,6 +4,11 @@ from utils import *
 variables: dict[str, int | float] = {}
 error_index: int = 0
 
+def print_error_message(error_message: str):
+	global error_index
+	print(error_message, file=sys.stderr)
+	error_index += 1
+
 def assign_rational_nums(user_input: str):
 	global error_index
 	if is_integer(user_input) or is_float(user_input):
@@ -11,31 +16,36 @@ def assign_rational_nums(user_input: str):
 		return
 	elif user_input.isalpha():
 		if user_input == 'i':
-			print(f'Error {error_index}: \'i\' cannot be assigned or used as a variable name.', file=sys.stderr)
-			error_index += 1
+			print_error_message(f'Error {error_index}: \'i\' cannot be assigned or used as a variable name.')
 			return
 		print(f'> {variables.get(user_input, 0)}')
 		return
-
+	
 	var_list: list[str] = user_input.strip().replace(' ', '').split('=')
-	if len(var_list) == 2:
-		var_name, value = var_list
-		if var_name.isalpha():
-			if var_name == 'i' or value == 'i':
-				print(f'Error {error_index}: \'i\' cannot be assigned or used as a variable name.', file=sys.stderr)
-				error_index += 1
-			elif is_integer(value) or is_float(value):
-				variables[var_name] = value
-				print(f'> {variables[var_name]}')
-			elif value.isalpha():
-				variables[var_name] = variables.get(value, 0)
-				print(f'> {variables[var_name]}')
-			else:
-				print(f'Error {error_index}: syntax error', file=sys.stderr)
-				error_index += 1
+	var_list.reverse()
+
+	first_item = var_list[0]
+    
+	if first_item == 'i':
+		print_error_message(f'Error {error_index}: \'i\' cannot be assigned or used as a variable name.')
+		return
+	elif not first_item.isalpha() and not (is_integer(first_item) or is_float(first_item)):
+		print_error_message(f'Error {error_index}: syntax error')
+		return
+
+	for var in var_list[1:]:
+		if var == 'i':
+			print_error_message(f'Error {error_index}: \'i\' cannot be assigned or used as a variable name.')
+			return
+		elif not var.isalpha():
+			print_error_message(f'Error {error_index}: syntax error')
+			return
+
+		if first_item.isalpha():
+			variables[var] = variables.get(first_item, 0)
+		elif is_integer(first_item) or is_float(first_item):
+			variables[var] = int(first_item) if is_integer(first_item) else float(first_item)
 		else:
-			print(f'Error {error_index}: syntax error', file=sys.stderr)
-			error_index += 1
-	else:
-		print(f'Error {error_index}: syntax error', file=sys.stderr)
-		error_index += 1
+			print_error_message(f'Error {error_index}: syntax error')
+			return
+	print(f'> {variables[var]}')
--- run.py
+++ run.py
@@ -9,9 +9,15 @@ signal.signal(signal.SIGINT, signal_handler)
 def main():
 	print('Welcome to Computorv2, the Python Calculator in command line!\n'
 		'To exit, type "exit" or "quit" and press Enter.')
+	history: list[str] = []
 	while True:
 		try:
 			user_input: str = input("> ")
+			if user_input == 'history':
+				for count, el in enumerate(history):
+					print(f'> {count}-> {el}')
+				continue
+			history.append(user_input)
 			if not user_input.strip():
 				continue
 			if user_input in ['exit', 'quit']:
